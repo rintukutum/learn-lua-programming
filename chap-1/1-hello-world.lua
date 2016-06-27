ce2cc3ddd8cd06de7dc083a7481930d91d33eff3
rintukutum <rintukutum@gmail.com>
2016-06-28 03:54:43 +0530

    added alternative iteractive mode!

--- chap-1/1-hello-world.lua
+++ chap-1/1-hello-world.lua
@@ -3,12 +3,20 @@
    It is similar like R-statistical programming language
    but differs in few such as 
    this chunk of comments will be written using # in R 
-]]
+--]]
 
+---[[
 print("Hello World!")
+--]]
 
 --[[
   command: lua program.lua
   result: Hello world
-]]
+--]]
 
+-- alternative in interactive mode
+--[[
+  hello = 'Hello world!';
+  -- useful for debugging
+  = hello;
+--]]
