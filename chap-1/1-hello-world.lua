1d1aadd873e20ec699f8bf92806a88f1cd5d66aa
rintukutum <rintukutum@gmail.com>
2016-06-28 03:23:55 +0530

    comment is added!

--- chap-1/1-hello-world.lua
+++ chap-1/1-hello-world.lua
@@ -1,1 +1,9 @@
+--[[
+   Hello world program in Lua programming
+   It is similar like R-statistical programming language
+   but differs in few such as 
+   this chunk of comments will be written using # in R
+   
+]]
+
 print("Hello World!")
