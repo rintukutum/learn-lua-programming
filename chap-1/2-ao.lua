278fd9262f721873a6c5199310e7d4f74ba870fd
rintukutum <rintukutum@gmail.com>
2016-06-28 03:41:11 +0530

    2-ao.lua added!

---
+++ chap-1/2-ao.lua
@@ -0,0 +1,15 @@
+-- Let us perform some arithmetic operations (ao)
+
+a = 10; -- variable a
+print('a =',a);
+b = 50; -- variable b
+print('b =',b);
+
+result = a + b; 
+print('a + b =', result);
+
+result = a * b;
+print('a * b =', result);
+
+result = a / b;
+print('a / b =', result);
