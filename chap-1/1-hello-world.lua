e8924dafbef8ba9e9c941b68b8d4f911de19f8e9
rintukutum <rintukutum@gmail.com>
2016-06-28 03:26:05 +0530

    result added

--- chap-1/1-hello-world.lua
+++ chap-1/1-hello-world.lua
@@ -2,8 +2,13 @@
    Hello world program in Lua programming
    It is similar like R-statistical programming language
    but differs in few such as 
-   this chunk of comments will be written using # in R
-   
+   this chunk of comments will be written using # in R 
 ]]
 
 print("Hello World!")
+
+--[[
+  command: lua program.lua
+  result: Hello world
+]]
+
