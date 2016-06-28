dba19d1bfcd8c615949bd147a1421dc237c16a14
rintukutum <rintukutum@gmail.com>
2016-06-28 06:44:44 +0530

    exercise-01 added

--- chap-1/exercise/ex-1.lua
+++ chap-1/exercise/ex-1.lua
@@ -1,8 +1,21 @@
+-- exercise 1
 -- function for factorial
 
 function fac (n)
-    if n == 0 then
-        result = 1
+    -- for non-negative integers
+    if n >= 0 then
+    -- convert into interger
+    n = math.floor(n);
+        if n == 0 then
+            return 1
+        else
+            return n * fac(n-1)
+        end
     else
-        result = n * (n-1)
-        
+        -- convert into interger
+        n = -math.floor(math.abs(n));
+        -- for negative integers
+        return (-1)^(-n-1) / fac(-n-1)
+    end
+end
+
